4bfc801549b25a3dd248fc68ef3f4d63bf879938
Alexey Ganzha <aganzha@yandex.ru>
2023-12-09 15:12:27 +0200

    proper highlight

--- src/main.rs
+++ src/main.rs
@@ -82,43 +82,43 @@ fn build_ui(app: &adw::Application) {
     txt.connect_move_cursor(move |view, step, count, _selection| {
         let buffer = view.buffer();
         let pos = buffer.cursor_position();
-        let mut iter = buffer.iter_at_offset(pos);
-        let mut clear_highlight = false;
+        let mut start_iter = buffer.iter_at_offset(pos);
         match step {
             gtk::MovementStep::LogicalPositions |
             gtk::MovementStep::VisualPositions => {
-                iter.forward_chars(count);
-                clear_highlight = !iter.has_tag(&tc);
+                start_iter.forward_chars(count);
             },
             gtk::MovementStep::Words => {
-                iter.forward_word_end();
-                clear_highlight = !iter.has_tag(&tc);
+                start_iter.forward_word_end();
             },
             gtk::MovementStep::DisplayLines |
             gtk::MovementStep::DisplayLineEnds |
             gtk::MovementStep::Paragraphs |
             gtk::MovementStep::ParagraphEnds => {
-                iter.forward_lines(count);
-                clear_highlight = !iter.has_tag(&tc);
+                start_iter.forward_lines(count);
             },
             gtk::MovementStep::Pages |
             gtk::MovementStep::BufferEnds |
             gtk::MovementStep::HorizontalPages => {
-                clear_highlight = true;
             },
             _ => todo!()
         }
         let start_mark = buffer.mark("start_highlight").unwrap();
         let end_mark = buffer.mark("end_highlight").unwrap();
-        println!("Soooooooooooooooooooo ? {:?}", clear_highlight);
-        if clear_highlight {
+        if start_iter.line() !=  buffer.iter_at_offset(pos).line() {
             buffer.remove_tag(
                 &tc,
                 &buffer.iter_at_mark(&start_mark),
                 &buffer.iter_at_mark(&end_mark)
-            );
-            //let end_iter = buffer.iter_at_offset(iter)
-            println!("REEEEMOOOOVE TAG");
+            );            
+            start_iter.set_line_offset(0);
+            let mut end_iter = buffer.iter_at_offset(start_iter.offset());
+            end_iter.forward_to_line_end();
+
+            buffer.move_mark(&start_mark, &start_iter);
+            buffer.move_mark(&end_mark, &end_iter);
+            buffer.apply_tag(&tc, &start_iter, &end_iter);
+
         }
     });
     let lorem: &str = "Untracked files (1)
@@ -147,7 +147,7 @@ c622f7f init";
     buffer.create_mark(Some("end_highlight"), &end_iter, false);    
     
     buffer.apply_tag(&tag, &start_iter, &end_iter);
-    println!("ADDED TAG {:?} : {:?} end offset", start_iter.offset(), end_iter.offset());
+    println!("ADDED TAG {:?}", buffer.slice(&start_iter, &end_iter, true), );
     stage.append(&txt);
 
     window.set_content(Some(&stage));
