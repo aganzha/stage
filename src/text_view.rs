2c2bceb4235a658e54dce566b43dab88d8a8e6bd
Alexey Ganzha <aganzha@yandex.ru>
2023-12-10 13:12:41 +0200

    apply highkight

--- src/text_view.rs
+++ src/text_view.rs
@@ -10,6 +10,7 @@ pub fn text_view_factory() ->  TextView {
         .build();
     let buffer = txt.buffer();
     let tag = TextTag::new(Some(HIGHLIGHT));
+    tag.set_background(Some("#f6fecd"));
     
     let event_controller = gtk::EventControllerKey::new();
     event_controller.connect_key_pressed({
@@ -34,21 +35,17 @@ pub fn text_view_factory() ->  TextView {
         move |gesture, _some, wx, wy| {
             gesture.set_state(gtk::EventSequenceState::Claimed);
             let (x, y) = txt.window_to_buffer_coords(gtk::TextWindowType::Text, wx as i32, wy as i32);
-            let buffer = txt.buffer();
             let maybe_iter = txt.iter_at_location(x, y);
             if maybe_iter.is_none() {
                 return;
             }
-            highlight_if_need(buffer, maybe_iter.unwrap());
-            println!("Box pressed! {:?} {:?} {:?} {:?}", wx, wy, x, y);
+            highlight_if_need(&txt, maybe_iter.unwrap());
+            let alloc = txt.allocation();
+            println!("Box pressed! {:?} {:?} {:?} {:?} == {:?}", wx, wy, x, y, alloc);
         }
     });
     
-    txt.add_controller(gesture);
-
-
-
-    tag.set_background(Some("#f6fecd"));
+    txt.add_controller(gesture);    
 
     txt.connect_move_cursor({
         move |view, step, count, _selection| {
@@ -75,11 +72,12 @@ pub fn text_view_factory() ->  TextView {
                 },
                 _ => todo!()
             }
-            highlight_if_need(buffer, start_iter);
+            highlight_if_need(view, start_iter);
         }
     });
     
-    let lorem: &str = "Untracked files (1)
+    let lorem: &str = "
+Untracked files (1)
 src/style.css
 
 Recent commits
@@ -104,13 +102,14 @@ c622f7f init";
     end_iter.forward_to_line_end();
     buffer.create_mark(Some(HIGHLIGHT_END), &end_iter, false);    
     
-    buffer.apply_tag(&tag, &start_iter, &end_iter);
-    println!("ADDED TAG {:?}", buffer.slice(&start_iter, &end_iter, true), );
+    highlight_if_need(&txt, start_iter);
+    
     txt
 }
 
-pub fn highlight_if_need(buffer: gtk::TextBuffer,
+pub fn highlight_if_need(view: &TextView,
                          mut start_iter: gtk::TextIter) {
+    let buffer = view.buffer();
     let start_mark = buffer.mark(HIGHLIGHT_START).unwrap();
     if start_iter.line() ==  buffer.iter_at_mark(&start_mark).line() {
         return;
@@ -122,10 +121,25 @@ pub fn highlight_if_need(buffer: gtk::TextBuffer,
         &buffer.iter_at_mark(&end_mark)
     );            
     start_iter.set_line_offset(0);
+    let start_pos = start_iter.offset();
     let mut end_iter = buffer.iter_at_offset(start_iter.offset());
     end_iter.forward_to_line_end();
 
+    let mut cnt = 0;
+    let  max_width = view.visible_rect().width();
+    while max_width >  view.iter_location(&end_iter).x() {
+        buffer.insert(&mut end_iter, " ");
+        cnt += 1;
+        if cnt > 100 {
+            break;
+        }
+    }
+    if cnt > 0 {
+        buffer.backspace(&mut end_iter, false, true);
+    }
+    let start_iter = buffer.iter_at_offset(start_pos);
     buffer.move_mark(&start_mark, &start_iter);
     buffer.move_mark(&end_mark, &end_iter);
-    buffer.apply_tag_by_name(HIGHLIGHT, &start_iter, &end_iter);    
+    buffer.apply_tag_by_name(HIGHLIGHT, &start_iter, &end_iter);
+
 }
