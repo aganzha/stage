2744a4a1c8b61f926d4b8047e9da991ee5d6db59
Alexey Ganzha <aganzha@yandex.ru>
2023-12-16 11:06:38 +0200

    display blobs

--- src/git.rs
+++ src/git.rs
@@ -1,5 +1,5 @@
-use std::env;
-use git2::{Repository, StatusOptions};
+use std::{env, str};
+use git2::{Repository, StatusOptions, ObjectType, Oid};
 use crate::glib::{Sender};
 use crate::gio;
 
@@ -8,7 +8,6 @@ fn get_current_repo() -> Result<Repository, String> {
         .map_err(|e| format!("can't get repo from executable {:?}", e))?;
     loop {        
         let path = path_buff.as_path();
-        println!("tryyyyyyyyyyyyyyyy {:?}", path);
         let repo_r = Repository::open(path);
         if let Ok(repo) =repo_r {
             return Ok(repo)
@@ -22,11 +21,11 @@ fn get_current_repo() -> Result<Repository, String> {
 }
 
 pub fn get_current_repo_status(sender: Sender<crate::Event>) {
-    println!("111111111111111111111111111111");
+
     if let Ok(repo) = get_current_repo() {
-        println!("22222222222222222222222222222");
         let path = repo.path();
-        sender.send(crate::Event::CurrentRepo(std::ffi::OsString::from(path))).expect("Could not send through channel");
+        sender.send(crate::Event::CurrentRepo(std::ffi::OsString::from(path)))
+            .expect("Could not send through channel");
         if let Ok(statuses) = repo.statuses(None) {
             for status_entry in statuses.iter() {
                 println!("Status path {:?}", status_entry.path());
@@ -34,17 +33,60 @@ pub fn get_current_repo_status(sender: Sender<crate::Event>) {
                     continue
                 }
                 let path = status_entry.path().unwrap();
-                if path == "target/" {
-                    // TODO! check .gitignore flags : Status(IGNORED)
-                    continue
+                // if path == "target/" {
+                //     // TODO! check .gitignore flags : Status(IGNORED)
+                //     continue
+                // }
+                // println!("flags : {:?}", status_entry.status());
+                // println!("head to index {:?}", status_entry.head_to_index());
+                // println!("index to workdir {:?}", status_entry.index_to_workdir());
+                // println!("");
+                // println!("->");
+                if let Some(workdir_delta) = status_entry.index_to_workdir() {
+                    let id = workdir_delta.old_file().id();
+                    if let Some(text) = blob_text_from_id(&repo, id) {
+                        println!("old_file");
+                        print!("{}", text);
+                    }
+                    // println!("id of diff file, which must be blob {:?}, zero? {:?}", id, id.is_zero());
+                    // if !id.is_zero() {
+                    //     let ob = repo.find_object(id, Some(ObjectType::Blob));
+                    //     println!("Ooooooooooooooob {:?}", ob);
+                    //     if let Ok(blob) = ob {
+                    //         let bl = blob.as_blob();
+                    //         if let Some(the_blob) = bl {
+                    //             let bytes = the_blob.content();
+                    //             //println!("blob? {:?}", str::from_utf8(bytes));
+                    //             let s = str::from_utf8(bytes).unwrap();
+                    //             print!("{}", s);
+                    //             println!("");
+                    //             println!("");
+                    //         }
+                    //     }                        
+                    // }
                 }
-                println!("flags : {:?}", status_entry.status());
-                println!("head to index {:?}", status_entry.head_to_index());
-                println!("index to workdir {:?}", status_entry.index_to_workdir());
             }
         }        
     }
 }
+
+pub fn blob_text_from_id(repo: &Repository, id: Oid) -> Option<String> {
+    if !id.is_zero() {
+        let ob = repo.find_object(id, Some(ObjectType::Blob));
+        println!("Ooooooooooooooob {:?}", ob);
+        if let Ok(blob) = ob {
+            let bl = blob.as_blob();
+            if let Some(the_blob) = bl {
+                let bytes = the_blob.content();
+                let s = str::from_utf8(bytes).unwrap();
+                return Some(String::from(s))
+            }
+        }                        
+    }
+    None
+}
+
+
 
 pub fn stage_changes(repo: Repository, _sender: Sender<crate::Event>) {
     // gio::spawn_blocking(|| {
