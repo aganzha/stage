fee019aa9bf67b63977463a0fea1e443303dc99f
Alexey Ganzha <aganzha@yandex.ru>
2023-12-31 00:33:11 +0200

    debug live programm

--- gdb.sh
+++ gdb.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rust-gdb target/debug/deps/stage-7f2a5ac6c9752efe --command=gdb.commands
+# rust-gdb target/debug/deps/stage-7f2a5ac6c9752efe --command=gdb.commands
+rust-gdb target/debug/stage --command=gdb.commands.run
