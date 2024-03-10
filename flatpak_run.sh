244e18bceebde62c78f7de68883c279826ed5d62
Alexey Ganzha <aganzha@yandex.ru>
2024-03-10 22:48:59 +0200

    run in home folder

--- flatpak_run.sh
+++ flatpak_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clear && RUST_BACKTRACE=1 RUST_LOG=debug flatpak run com.github.aganzha.stage $@
+clear && RUST_BACKTRACE=1 RUST_LOG=debug flatpak run --filesystem=home com.github.aganzha.stage $@
