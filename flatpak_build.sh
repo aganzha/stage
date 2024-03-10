e2f1463c9dc4908a874aad73325d97bf28859a16
Alexey Ganzha <aganzha@yandex.ru>
2024-03-10 23:06:37 +0200

    changed scripts after shares in manifest

--- flatpak_build.sh
+++ flatpak_build.sh
@@ -1,1 +1,2 @@
-#!/bin/bash flatpak-builder --repo=flatpak_target flatpak_build com.github.aganzha.stage.json --force-clean
+#!/bin/bash
+flatpak-builder --repo=flatpak_target flatpak_build com.github.aganzha.stage.json --force-clean
--- flatpak_run.sh
+++ flatpak_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clear && RUST_BACKTRACE=1 RUST_LOG=debug flatpak run --filesystem=home --socket=ssh-auth --socket=gpg-agent --share=network com.github.aganzha.stage $@
+clear && RUST_BACKTRACE=1 RUST_LOG=debug flatpak run com.github.aganzha.stage $@
