a6d149dc148d6f56f83ff73f5aa58af344f74223
Alexey Ganzha <aganzha@yandex.ru>
2024-05-01 21:19:29 +0200

    merge branch refactor_header_bar into master

--- src/status_view/textview.rs
+++ src/status_view/textview.rs
@@ -83,7 +83,7 @@ impl CharView for TextView {
     }
 }
 
-pub fn text_view_factory(
+pub fn factory(
     sndr: Sender<crate::Event>,
     text_view_width: Rc<RefCell<(i32, i32)>>,
 ) -> TextView {
