ecb643b762561cdc9adddc06933b757aa5cf622b
au.ganzha <au.ganzha@tensor.ru>
2024-05-14 12:34:08 +0200

    try resolve v1

--- src/git.rs
+++ src/git.rs
@@ -1297,7 +1297,9 @@ pub fn push(
                 .expect("cant send through channel");
         }
         Err(error) => {
-            panic!("{}", error);
+            sender
+                .send_blocking(crate::Event::Toast(String::from(error.message())))
+                .expect("cant send through channel");
         }
     }
 }
