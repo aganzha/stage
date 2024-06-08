2737a608bc8e6fb84acdbd818fa41858873f5472
Alexey Ganzha <aganzha@yandex.ru>
2024-06-08 14:22:23 +0200

    completed scheme toggle with storage

--- src/debug.rs
+++ src/debug.rs
@@ -1,4 +1,7 @@
 use log::debug;
+use std::cell::RefCell;
+use std::sync::RwLock;
+use std::sync::Mutex;
 use gtk4::prelude::*;
 use gtk4::{
     gdk, gio, glib, Button, EventControllerKey, Label, ScrolledWindow,
@@ -9,15 +12,16 @@ use libadwaita::prelude::*;
 use libadwaita::{HeaderBar, ToolbarView, Window, StyleManager, ColorScheme};
 
 use crate::gio::MenuModel;
-    
-pub fn debug(app_window: &impl IsA<Gtk4Window>,) {
-    debug!("------------------->");
+use async_channel::Sender;
+
+pub fn debug(app_window: &impl IsA<Gtk4Window>, mut stored_theme: String, sender: Sender<crate::Event>) {
+    debug!("-------------------> {:?}", stored_theme);
     let window = Window::builder()
         .transient_for(app_window)
         .default_width(640)
         .default_height(480)
         .build();
-    
+
     let bx = Box::builder()
         .orientation(Orientation::Vertical)
         .margin_top(2)
@@ -30,7 +34,7 @@ pub fn debug(app_window: &impl IsA<Gtk4Window>,) {
 
     // let popover_menu = PopoverMenu::builder()
     //     .build();
-    
+
     let menu_model = gio::Menu::new();
     let menu_item = gio::MenuItem::new(Some("theme_label"), Some("win.menu::1"));
 
@@ -38,7 +42,7 @@ pub fn debug(app_window: &impl IsA<Gtk4Window>,) {
 
 
     menu_item.set_attribute_value("custom", Some(&theme_id));
-    
+
     menu_model.insert_item(0, &menu_item);
 
     let menu_item = gio::MenuItem::new(Some("just_label"), Some("win.menu::2"));
@@ -47,10 +51,10 @@ pub fn debug(app_window: &impl IsA<Gtk4Window>,) {
     menu_item.set_attribute_value("custom", Some(&label_id));
     // menu_item.set_attribute_value("label", Some(&label_id));
     menu_model.insert_item(1, &menu_item);
-    
-    
+
+
     let popover_menu = PopoverMenu::from_model(Some(&menu_model));
-    
+
     let selector = Label::new(Some("selector"));
     // https://gtk-rs.org/gtk4-rs/stable/latest/docs/gtk4/struct.PopoverMenu.html#method.add_child
     popover_menu.add_child(&selector, "theme");
@@ -59,69 +63,140 @@ pub fn debug(app_window: &impl IsA<Gtk4Window>,) {
     // https://gtk-rs.org/gtk4-rs/stable/latest/docs/gtk4/struct.PopoverMenu.html#method.add_child
     popover_menu.add_child(&la, "label");
 
+
+    if stored_theme.is_empty() {
+        stored_theme = "follow".to_string();
+    }
+
     let burger = MenuButton::builder()
         .popover(&popover_menu)
         .icon_name("open-menu-symbolic")
         .build();
-
 
     let mine_selector = Box::builder()
         .orientation(Orientation::Horizontal)
         .css_name("mine_selector")
         .build();
 
-    let transform = |b: &glib::Binding, is_active: bool| {        
-        if is_active {
-            let tb = b.source().unwrap();
-            let tb = tb.downcast_ref::<Widget>().expect("cant get widget");
-            let theme = match tb.widget_name().as_str() {
-                "follow" => ColorScheme::Default,
-                "light" => ColorScheme::ForceLight,
-                "dark" => ColorScheme::ForceDark,
-                n => todo!("whats the name? {:?}", n)
-            };
-            let manager = StyleManager::default();
-            manager.set_color_scheme(theme);
-            Some("object-select-symbolic")
-        } else {
-            Some("")
+    let mut first_toggle: Option<ToggleButton> = None;
+    for id in ["follow", "light", "dark"] {
+        let toggle = ToggleButton::builder()
+            .active(false)
+            .icon_name("")
+            .name(id)
+            .css_classes(vec![id])
+            .margin_end(10)
+            .build();
+        if stored_theme == id {
+            toggle.set_icon_name("object-select-symbolic");
+            toggle.set_active(true);
         }
+        toggle.last_child().unwrap().set_halign(Align::Center);
+        toggle.bind_property("active", &toggle, "icon_name").transform_to({
+            let sender = sender.clone();
+            move |_, is_active: bool| {
+                debug!("TTTTTTTTTTTTTTTTTTT {} {}", id, is_active);
+                if is_active {
+                    let theme = match id {
+                        "follow" => ColorScheme::Default,
+                        "light" => ColorScheme::ForceLight,
+                        "dark" => ColorScheme::ForceDark,
+                        n => todo!("whats the name? {:?}", n)
+                    };
+                    let manager = StyleManager::default();
+                    manager.set_color_scheme(theme);
+                    sender.send_blocking(
+                        crate::Event::StoreSettings("theme".to_string(),
+                                                    id.to_string())
+                    ).expect("cant send through sender");
+                    debug!("seeeeeeeeeeeeeeeeet icon {}", id);
+                    Some("object-select-symbolic")
+                        
+                } else {
+                    debug!("uuuuuuuuuuuuuuuuuuuunset icon {}", id);
+                    Some("")
+                }
+            }}).build();
+        mine_selector.append(&toggle);
+        if let Some(ref ft) = first_toggle {
+            toggle.set_group(Some(ft));
+        } else {
+            first_toggle.replace(toggle);
+        }
+
     };
-    let follow = ToggleButton::builder()
-        .active(true)
-        .name("follow")
-        .icon_name("object-select-symbolic")
-        .css_classes(vec!["follow"])
-        .margin_end(10)
-        .build();
-    follow.bind_property("active", &follow, "icon_name")
-        .transform_to(transform).build();
 
-    follow.last_child().unwrap().set_halign(Align::Center);
-    let light = ToggleButton::builder()
-        .icon_name("")
-        .name("light")
-        .css_classes(vec!["light"])
-        .margin_end(10)
-        .group(&follow).build();
-    light.bind_property("active", &follow, "icon_name")
-        .transform_to(transform).build();
 
-    light.last_child().unwrap().set_halign(Align::Center);
+    // let follow = ToggleButton::builder()
+    //     .active(true)
+    //     .icon_name("")
+    //     .name("follow")
+    //     .css_classes(vec!["follow"])
+    //     .margin_end(10)
+    //     .build();
+    // if stored_theme == "default" {
+    //     follow.set_icon_name("object-select-symbolic");
+    //     follow.set_active(true);
+    // }
 
-    let dark = ToggleButton::builder()
-        .icon_name("")
-        .name("dark")
-        .css_classes(vec!["dark"])
-        .group(&follow).build();
-    dark.bind_property("active", &follow, "icon_name")
-        .transform_to(transform).build();
+
+    // follow.last_child().unwrap().set_halign(Align::Center);
+
+    // let light = ToggleButton::builder()
+    //     .icon_name("")
+    //     .name("light")
+    //     .css_classes(vec!["light"])
+    //     .margin_end(10)
+    //     .group(&follow).build();
+    // if stored_theme == "light" {
+    //     light.set_icon_name("object-select-symbolic");
+    //     light.set_active(true);
+    // }
+
+    // light.last_child().unwrap().set_halign(Align::Center);
+
+    // let dark = ToggleButton::builder()
+    //     .icon_name("")
+    //     .name("dark")
+    //     .css_classes(vec!["dark"])
+    //     .group(&follow).build();
+    // if stored_theme == "light" {
+    //     light.set_icon_name("object-select-symbolic");
+    //     light.set_active(true);
+    // }
+    // dark.last_child().unwrap().set_halign(Align::Center);
+
+    // for w in [&follow, &light, &dark] {
+    //     w.bind_property("active", w, "icon_name").transform_to({
+    //         let sender = sender.clone();
+    //         move |b: &glib::Binding, is_active: bool| {
+    //             if is_active {
+    //                 let tb = b.source().unwrap();
+    //                 let tb = tb.downcast_ref::<Widget>().expect("cant get widget");
+    //                 let chosen = tb.widget_name();
+    //                 let theme = match chosen.as_str() {
+    //                     "follow" => ColorScheme::Default,
+    //                     "light" => ColorScheme::ForceLight,
+    //                     "dark" => ColorScheme::ForceDark,
+    //                     n => todo!("whats the name? {:?}", n)
+    //                 };
+    //                 let manager = StyleManager::default();
+    //                 manager.set_color_scheme(theme);
+    //                 sender.send_blocking(
+    //                     crate::Event::StoreSettings("theme".to_string(),
+    //                                                 chosen.to_string())
+    //                 ).expect("cant send through sender");
+    //                 Some("object-select-symbolic")
+                        
+    //             } else {
+    //                 Some("")
+    //             }
+    //         }}).build();
+    // }
     
-    dark.last_child().unwrap().set_halign(Align::Center);
-
-    mine_selector.append(&follow);
-    mine_selector.append(&light);
-    mine_selector.append(&dark);
+    // mine_selector.append(&follow);
+    // mine_selector.append(&light);
+    // mine_selector.append(&dark);
 
 
 
@@ -131,12 +206,12 @@ pub fn debug(app_window: &impl IsA<Gtk4Window>,) {
 
     let hb = HeaderBar::builder().build();
     hb.pack_end(&burger);
-    
+
     let tb = ToolbarView::builder().content(&bx).build();
     tb.add_top_bar(&hb);
 
     window.set_content(Some(&tb));
 
-    
+
     window.present();
 }
