5f4fa73791d3d3043e8d637a776bb087b23708ae
Alexey Ganzha <aganzha@yandex.ru>
2024-07-13 11:08:21 +0200

    search for tags

--- src/git/tag.rs
+++ src/git/tag.rs
@@ -17,11 +17,13 @@ pub struct Tag {
 
 impl Tag {
     pub fn new(oid: git2::Oid, name: String, commit: CommitLog, message: String) -> Tag {
+        let mut encoded = String::from("");
+        html_escape::encode_safe_to_string(message, &mut encoded);
         Tag {
             oid,
             name,
             commit,
-            message
+            message: encoded
         }
     }
 }
@@ -41,7 +43,7 @@ impl Default for Tag {
 pub const TAG_PAGE_SIZE: usize = 100;
 
 pub fn get_tag_list(path: PathBuf, start_oid: Option<git2::Oid>, search_term: Option<String>) -> Result<Vec<Tag>, git2::Error> {
-    info!("get_tag_list {:?}", start_oid);
+    info!("get_tag_list {:?} {:?}", start_oid, search_term);
     let repo = git2::Repository::open(path.clone())?;
     let mut result = Vec::new();
     let mut cnt = 0;
@@ -56,10 +58,20 @@ pub fn get_tag_list(path: PathBuf, start_oid: Option<git2::Oid>, search_term: Op
         if let Ok(tag) =  repo.find_tag(oid) {
             if let Ok(ob) = tag.target() {
                 if let Ok(commit) = ob.peel_to_commit() {                    
+                    let tag_name = String::from_utf8_lossy(name).to_string();
+                    if let Some(look_for) = &search_term {
+                        if tag_name.contains(look_for)
+                            || tag.message().unwrap_or("").contains(look_for)
+                            || commit.message().unwrap_or("").contains(look_for)
+                        {
+                        } else {
+                            return true
+                        }
+                    }
                     let commit_log = CommitLog::from_log(commit, CommitRelation::None);
                     result.push(Tag::new(
                         oid,
-                        String::from_utf8_lossy(name).to_string(),
+                        tag_name,
                         commit_log,
                         String::from(tag.message().unwrap_or(""))
                     ));
