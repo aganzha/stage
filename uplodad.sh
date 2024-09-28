c92e5e76e600b5358739010ae0cb1791beed7fb6
Alexey Ganzha <aganzha@yandex.ru>
2024-09-28 09:10:42 +0200

    desktop and metainfo

--- uplodad.sh
+++ uplodad.sh
@@ -1,2 +1,3 @@
 #/bin/bash
-aws s3 cp stage.flatpakref s3://www.aganzha.online/
+# aws s3 cp stage.flatpakref s3://www.aganzha.online/
+aws s3 cp --recursive flatpak_target s3://www.aganzha.online/stage_flatpak_repo/
