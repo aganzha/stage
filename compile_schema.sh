8b74407f67145e530b99473e555cb6872f9da79d
Alexey Ganzha <aganzha@yandex.ru>
2024-10-14 01:29:04 +0200

    latest changes in ID. build works

--- compile_schema.sh
+++ compile_schema.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-glib-compile-schemas ./src && cp ./src/gschemas.compiled ./target/debug
+glib-compile-schemas ./src && cp ./src/gschemas.compiled ./target/debug && cp ./src/gschemas.compiled ./target/release
