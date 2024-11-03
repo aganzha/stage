89d4ed4e5dc2436b8a423a69a07322289b780803
Alexey Ganzha <aganzha@yandex.ru>
2024-11-04 01:14:43 +0200

    try fix test runner

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# ./test.sh --test choose_conflict_side
-clear && RUST_BACKTRACE=1 RUST_LOG=debug RUST_TEST_THREADS=1 cargo test -- --test-threads=1 --nocapture $@
+RUST_BACKTRACE=1 RUST_LOG=debug RUST_TEST_THREADS=1 cargo test -- --test-threads=1 --nocapture $@
