e234ec1436b844810465940c9917a70b84322102
Alexey Ganzha <aganzha@yandex.ru>
2024-11-04 01:06:17 +0200

    try run tests

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # ./test.sh --test choose_conflict_side
-clear && RUST_BACKTRACE=1 RUST_LOG=debug RUST_TEST_THREADS=0 cargo test -- --test-threads=1 --nocapture $@
+clear && RUST_BACKTRACE=1 RUST_LOG=debug RUST_TEST_THREADS=1 cargo test -- --test-threads=1 --nocapture $@
