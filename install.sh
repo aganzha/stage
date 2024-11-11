cab49d430f5e8632fd357e3d07142d01eb10f652
Alexey Ganzha <aganzha@yandex.ru>
2024-11-12 01:05:35 +0200

    kill excessive icon

--- install.sh
+++ install.sh
@@ -11,5 +11,4 @@ install -Dm644 ./icons/128x128/io.github.aganzha.Stage.png -t /app/share/icons/h
 install -Dm644 ./icons/256x256/io.github.aganzha.Stage.png -t /app/share/icons/hicolor/256x256/apps
 install -Dm644 ./icons/512x512/io.github.aganzha.Stage.png -t /app/share/icons/hicolor/512x512/apps
 install -Dm644 ./icons/io.github.aganzha.Stage.svg -t /app/share/icons/hicolor/scalable/apps
-install -Dm644 ./icons/io.github.aganzha.Stage.Devel.svg -t /app/share/icons/hicolor/scalable/apps
 install -Dm644 ./icons/io.github.aganzha.Stage-symbolic.svg -t /app/share/icons/hicolor/symbolic/apps
