70fbd94b6885746972d9ef32c820968fa362c49e
Alexey Ganzha <aganzha@yandex.ru>
2024-11-18 13:05:35 +0200

    fixed warnings

--- src/tests.rs
+++ src/tests.rs
@@ -1,14 +1,22 @@
 // SPDX-FileCopyrightText: 2024 Aleksey Ganzha <aganzha@yandex.ru>
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+#[cfg(test)]
 use crate::status_view::tags;
+#[cfg(test)]
 use gtk4::prelude::*;
+#[cfg(test)]
 use gtk4::TextBuffer;
+#[cfg(test)]
 use log::debug;
+#[cfg(test)]
 use std::sync::Once;
 
+#[cfg(test)]
 static INIT: Once = Once::new();
 
+#[cfg(test)]
 pub fn initialize() -> TextBuffer {
     INIT.call_once(|| {
         env_logger::builder().format_timestamp(None).init();
