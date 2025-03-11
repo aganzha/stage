1be366ec4f32a5d25ab1551183891fc7c102ba05
au.ganzha <au.ganzha@tensor.ru>
2025-03-11 11:51:18 +0200

    custom diff

--- src/git.rs
+++ src/git.rs
@@ -14,8 +14,8 @@ use crate::branch::BranchData;
 use crate::commit::CommitRepr;
 use crate::gio;
 use crate::status_view::view::View;
-
 use async_channel::Sender;
+use std::fs::read;
 
 use chrono::{DateTime, FixedOffset};
 use git2::build::CheckoutBuilder;
@@ -30,7 +30,7 @@ use regex::Regex;
 use std::fmt;
 use std::num::ParseIntError;
 use std::ops::{Add, Sub};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::str::{from_utf8, FromStr};
 use std::{collections::HashSet, str};
 
@@ -40,6 +40,7 @@ pub fn make_diff_options() -> DiffOptions {
     opts.minimal(true);
     opts
 }
+use similar::TextDiff;
 
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct HunkLineNo(u32);
@@ -845,10 +846,13 @@ pub fn get_conflicted_v1(
     }
     let mut missing_theirs: Vec<git2::IndexEntry> = Vec::new();
     let mut has_conflicts = false;
+    let mut conflict_paths = Vec::new();
     for conflict in conflicts {
         let conflict = conflict.unwrap();
         if let Some(our) = conflict.our {
-            opts.pathspec(String::from_utf8(our.path).unwrap());
+            let pth = String::from_utf8(our.path).unwrap();
+            opts.pathspec(pth.clone());
+            conflict_paths.push(pth);
             has_conflicts = true;
         } else {
             missing_theirs.push(conflict.their.unwrap())
@@ -875,6 +879,7 @@ pub fn get_conflicted_v1(
     if !has_conflicts {
         return None;
     }
+
     let ob = repo.revparse_single("HEAD^{tree}").expect("fail revparse");
     let current_tree = repo.find_tree(ob.id()).expect("no working tree");
     let git_diff = repo
@@ -886,76 +891,90 @@ pub fn get_conflicted_v1(
     if diff.is_empty() {
         return None;
     }
-    // if intehunk is unknown it need to check missing hunks
-    // (either ours or theirs could go to separate hunk)
-    // and recreate diff to accomodate both ours and theirs in single hunk
-    if let Some(interhunk) = interhunk {
-        diff.interhunk.replace(interhunk);
-    } else {
-        // this vec store tuples with last line_no of prev hunk
-        // and first line_no of next hunk
-        let mut hunks_to_join = Vec::new();
-        let mut prev_conflict_line: Option<HunkLineNo> = None;
-        for file in &diff.files {
-            for hunk in &file.hunks {
-                let (first_marker, last_marker) =
-                    hunk.lines.iter().fold((None, None), |acc, line| {
-                        match (acc.0, acc.1, &line.kind) {
-                            (None, _, LineKind::ConflictMarker(m)) => (Some(m), Some(m)),
-                            (Some(_), _, LineKind::ConflictMarker(m)) => (acc.0, Some(m)),
-                            _ => acc,
-                        }
-                    });
-                match (first_marker, last_marker) {
-                    (None, _) => {
-                        // hunk without conflicts?
-                        // just skip it
-                    }
-                    (Some(_), None) => {
-                        panic!("imposible case");
-                    }
-                    (Some(first), Some(last)) if first == MARKER_THEIRS || first == MARKER_VS => {
-                        // hunk is not started with ours
-                        // store prev hunk last line and this hunk start to join em
-                        hunks_to_join.push((prev_conflict_line.unwrap(), hunk.old_start));
-                        if last == MARKER_OURS {
-                            prev_conflict_line
-                                .replace(HunkLineNo::new(hunk.old_start.as_u32() + hunk.old_lines));
-                        } else {
-                            prev_conflict_line = None;
-                        }
-                    }
-                    (_, Some(m)) if m != MARKER_THEIRS => {
-                        // hunk is not ended with theirs
-                        // store prev hunk last line and this hunk start to join em
-                        assert!(prev_conflict_line.is_none());
-                        prev_conflict_line
-                            .replace(HunkLineNo::new(hunk.old_start.as_u32() + hunk.old_lines));
-                    }
-                    (Some(start), Some(end)) => {
-                        assert!(start == MARKER_OURS);
-                        assert!(end == MARKER_THEIRS);
-                    }
-                }
-            }
-        }
-        if !hunks_to_join.is_empty() {
-            let interhunk = hunks_to_join
-                .iter()
-                .fold(HunkLineNo::new(0), |acc, from_to| {
-                    if acc < from_to.1 - from_to.0 {
-                        return from_to.1 - from_to.0;
-                    }
-                    acc
-                });
-            opts.interhunk_lines(interhunk.as_u32());
-            let git_diff = repo
-                .diff_tree_to_workdir(Some(&current_tree), Some(&mut opts))
-                .expect("cant get diff");
-            diff = make_diff(&git_diff, DiffKind::Conflicted);
-            diff.interhunk.replace(interhunk.as_u32());
-        }
+    debug!("_____________________________________{:?}", current_tree);
+    for path in conflict_paths {
+        let abs_file_path = repo.path().parent().unwrap().join(Path::new(&path));
+        debug!("file path of conflict {:?}", abs_file_path);
+        let entry = current_tree.get_path(Path::new(&path)).expect("no entry");
+        let ob = entry.to_object(&repo).expect("no object");
+        let blob = ob.as_blob().expect("no blob");
+        let tree_content = String::from_utf8_lossy(blob.content());
+        let file_bytes = read(abs_file_path).expect("no file");
+        let workdir_content = String::from_utf8_lossy(&file_bytes);
+        let text_diff = TextDiff::from_lines(&tree_content, &workdir_content);
+        let unified_diff = text_diff.unified_diff(); //.context_radius(3).header("old file", "new file");
+        debug!("____________________________ {}", unified_diff);
     }
+    // // if intehunk is unknown it need to check missing hunks
+    // // (either ours or theirs could go to separate hunk)
+    // // and recreate diff to accomodate both ours and theirs in single hunk
+    // if let Some(interhunk) = interhunk {
+    //     diff.interhunk.replace(interhunk);
+    // } else {
+    //     // this vec store tuples with last line_no of prev hunk
+    //     // and first line_no of next hunk
+    //     let mut hunks_to_join = Vec::new();
+    //     let mut prev_conflict_line: Option<HunkLineNo> = None;
+    //     for file in &diff.files {
+    //         for hunk in &file.hunks {
+    //             let (first_marker, last_marker) =
+    //                 hunk.lines.iter().fold((None, None), |acc, line| {
+    //                     match (acc.0, acc.1, &line.kind) {
+    //                         (None, _, LineKind::ConflictMarker(m)) => (Some(m), Some(m)),
+    //                         (Some(_), _, LineKind::ConflictMarker(m)) => (acc.0, Some(m)),
+    //                         _ => acc,
+    //                     }
+    //                 });
+    //             match (first_marker, last_marker) {
+    //                 (None, _) => {
+    //                     // hunk without conflicts?
+    //                     // just skip it
+    //                 }
+    //                 (Some(_), None) => {
+    //                     panic!("imposible case");
+    //                 }
+    //                 (Some(first), Some(last)) if first == MARKER_THEIRS || first == MARKER_VS => {
+    //                     // hunk is not started with ours
+    //                     // store prev hunk last line and this hunk start to join em
+    //                     hunks_to_join.push((prev_conflict_line.unwrap(), hunk.old_start));
+    //                     if last == MARKER_OURS {
+    //                         prev_conflict_line
+    //                             .replace(HunkLineNo::new(hunk.old_start.as_u32() + hunk.old_lines));
+    //                     } else {
+    //                         prev_conflict_line = None;
+    //                     }
+    //                 }
+    //                 (_, Some(m)) if m != MARKER_THEIRS => {
+    //                     // hunk is not ended with theirs
+    //                     // store prev hunk last line and this hunk start to join em
+    //                     assert!(prev_conflict_line.is_none());
+    //                     prev_conflict_line
+    //                         .replace(HunkLineNo::new(hunk.old_start.as_u32() + hunk.old_lines));
+    //                 }
+    //                 (Some(start), Some(end)) => {
+    //                     assert!(start == MARKER_OURS);
+    //                     assert!(end == MARKER_THEIRS);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     if !hunks_to_join.is_empty() {
+    //         let interhunk = hunks_to_join
+    //             .iter()
+    //             .fold(HunkLineNo::new(0), |acc, from_to| {
+    //                 if acc < from_to.1 - from_to.0 {
+    //                     return from_to.1 - from_to.0;
+    //                 }
+    //                 acc
+    //             });
+    //         opts.interhunk_lines(interhunk.as_u32());
+    //         let git_diff = repo
+    //             .diff_tree_to_workdir(Some(&current_tree), Some(&mut opts))
+    //             .expect("cant get diff");
+    //         diff = make_diff(&git_diff, DiffKind::Conflicted);
+    //         diff.interhunk.replace(interhunk.as_u32());
+    //     }
+    // }
     Some(diff)
 }
 
