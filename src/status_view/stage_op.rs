669a5e3a2d424e2b327d77936159afbce85a1a27
Alexey Ganzha <aganzha@yandex.ru>
2025-03-23 22:03:24 +0200

    proceed with conflict finalize. fixed regexp bug

--- src/status_view/stage_op.rs
+++ src/status_view/stage_op.rs
@@ -295,11 +295,7 @@ impl Status {
                                     // conflicts are resolved in branch above
                                     info!("cleanup_last_conflict_for_file");
                                     gio::spawn_blocking({
-                                        move || {
-                                            merge::cleanup_last_conflict_for_file(
-                                                path, file_path, sender,
-                                            )
-                                        }
+                                        move || merge::try_finalize_conflict(path, sender)
                                     })
                                     .await
                                     .unwrap_or_else(|e| {
