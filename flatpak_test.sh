576b89e8aca4fdcc590c7470bd8dc5de5d539e68
Alexey Ganzha <aganzha@yandex.ru>
2025-04-06 09:42:50 +0200

    try pass tests 15

--- flatpak_test.sh
+++ flatpak_test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+./compile_resources.sh
+./compile_schema.sh
 RUST_BACKTRACE=1 RUST_LOG=debug OUT_DIR=. cargo test --no-run -- --nocapture
 find target/debug/deps/ -type f -executable -name 'stage*' -exec '{}' ';'
