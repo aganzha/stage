80a0cab6d994c0b85001c1d45166f34909cd842e
Alexey Ganzha <aganzha@yandex.ru>
2025-06-13 15:41:19 +0200

    changes for debian

---
+++ debbuild.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+original_name="stage"
+name="stage-git-gui"
+version="0.1.21"
+release=1
+full_id="io.github.aganzha.Stage"
+tar_name="$name"_"$version".orig.tar.xz
+rm ../$tar_name
+sed -i "s/name = \"$original_name\"/name = \"$name\"/" Cargo.toml
+sed -i "s|Exec=$original_name|Exec=$name|g" $full_id.desktop
+tar cJvf $tar_name --exclude-vcs --exclude=target --exclude='*.sh' --exclude=target --exclude='.pc' --exclude='debian' --exclude='*~' --exclude='#*#' --exclude=$tar_name .
+mv $tar_name ../
+debuild -S -kD721B759479BF5233A2FAC54196584E65F8849A1
+#git checkout Cargo.toml
+#git checkout $full_id.desktop
+# dput -d ppa:aganzha/stage stage-git-gui_0.1.21-1_source.changes 
+
+
+# debuild -S
+# debuild -S -kD721B759479BF5233A2FAC54196584E65F8849A1
+# dpkg-buildpackage -us -uc
+
+# cd ../
+# changes_name="$name"_"$version"_amd64.changes
+# debsign -k D721B759479BF5233A2FAC54196584E65F8849A1 $changes_name
+# dput -d stage-git-gui_0.1.21-1_amd64.changes
+
+# ooooooooooooooooooooold --------------------------
+# apt-get install dh-cargo debhelper cargo
+# changes to Cargo.toml  - put in rules.
+# put proper info in debian/changelog
+# dh_make -s --createorig
+# dh_clean
+# dpkg-buildpackage -us -uc
+# debsign -k D721B759479BF5233A2FAC54196584E65F8849A1 stage-git-gui_0.1.21-1_amd64.changes
+# dput stage stage-git-gui_0.1.21-1_amd64.changes
