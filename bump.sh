4586006f862db81f8d1e80a3c95550eca88de9f4
Alexey Ganzha <aganzha@yandex.ru>
2025-06-21 07:54:29 +0200

    changed debbuild

--- bump.sh
+++ bump.sh
@@ -5,3 +5,4 @@ sed -i "s/$current_version/$new_version/g" ./Cargo.toml
 sed -i "s/$current_version/$new_version/g" ./io.github.aganzha.Stage.json
 sed -i "s/$current_version/$new_version/g" ./io.github.aganzha.Stage.metainfo.xml
 sed -i "s/$current_version/$new_version/g" ./rpmbuild.sh
+sed -i "1s/.*/stage-git-gui ($new_version-1) plucky; urgency=medium/" debian/changelog
