e988e76fbb649c642b5a18ad7d6a64f1f81850cb
Alexey Ganzha <aganzha@yandex.ru>
2026-01-25 12:07:02 +0200

    bump version to 24

--- bump.sh
+++ bump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-current_version="0.1.22"
-new_version="0.1.23"
+current_version="0.1.23"
+new_version="0.1.24"
 sed -i "s/$current_version/$new_version/g" ./Cargo.toml
 sed -i "s/$current_version/$new_version/g" ./io.github.aganzha.Stage.json
 sed -i "s/$current_version/$new_version/g" ./io.github.aganzha.Stage.metainfo.xml
